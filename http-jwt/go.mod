37d9a5442063f80b3e5e0c8ea9fde998757f68a5
Mikael <mikaelmr@gmail.com>
2023-03-07 15:37:53 +1300

    JWT

---
+++ http-jwt/go.mod
@@ -0,0 +1,3 @@
+module example/http-jwt
+
+go 1.20
---
+++ http-jwt/login.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type LoginRequest struct {
+	Password string `json:"password"`
+}
+type LoginResponse struct {
+	Token string `json:"token"`
+}
+
+// the first string type can be omitted
+func doLoginRequest(client http.Client, requestURL, password string) (string, error) {
+	LoginRequest := LoginRequest{
+		Password: password,
+	}
+	body, err := json.Marshal(LoginRequest)
+	if err != nil {
+		return "", fmt.Errorf("Marshall Error: %s", err)
+	}
+
+	// use NewBuffer that implements a "Read" function
+	response, err := client.Post(requestURL, "application/json", bytes.NewBuffer(body))
+
+	if err != nil {
+		return "", fmt.Errorf("HTTP Post error: %s", err)
+	}
+
+	defer response.Body.Close()
+
+	resBody, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		return "", fmt.Errorf("ReadAll error: %s", err)
+	}
+
+	if response.StatusCode != 200 {
+		return "", fmt.Errorf("Invalid output (HTTP Status Code: %d): %s", response.StatusCode, resBody)
+	}
+
+	if !json.Valid(resBody) {
+		return "", RequestError{
+			HTTPCode: response.StatusCode,
+			Body:     string(resBody),
+			Err:      fmt.Sprintf("No valid JSON returned"),
+		}
+	}
+
+	var loginResponse LoginResponse
+	err = json.Unmarshal(resBody, &loginResponse)
+	if err != nil {
+		return "", RequestError{
+			HTTPCode: response.StatusCode,
+			Body:     string(resBody),
+			Err:      fmt.Sprintf("Page Unmarshal error: %s", err),
+		}
+	}
+
+	return loginResponse.Token, nil
+}
